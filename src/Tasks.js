40de3f2276130005c7fbe4ce6c0ff1ddbd5dbf84
Re9iNee <attarzadeh76@gmail.com>
2021-08-07 14:11:23 +0430

    tasks sent to Tasks Comp as a Props

--- src/Tasks.js
+++ src/Tasks.js
@@ -3,34 +3,10 @@ import "./styles/Tasks.css";
 
 
 class Tasks extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            tasks: [{
-              title: "Wash Clothes",
-              id: 1,
-              children: [{
-                title: "Sport Clothes",
-                id: 2,
-              }, {
-                title: "Casuals",
-                id: 3
-              }]
-            }, {
-              title: "Groceries",
-              id: 4,
-              children: []
-            }, {
-              title: "Fix Mac",
-              id: 5,
-              children: []
-            }]
-        };
-    }
     render() {
         return (
             <ul>
-                {this.state.tasks.map((task) => 
+                {this.props.tasks.map((task) => 
                     <li tabIndex="0" key={task.id}>
                         {task.title}
                         {task.children.length > 0 && 
