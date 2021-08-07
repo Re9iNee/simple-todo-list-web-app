b7dfb89cde5ec6175416e41a73723f47cd7ceb46
Re9iNee <attarzadeh76@gmail.com>
2021-08-07 13:45:02 +0430

    added localStorage database file - finished writing

---
+++ src/database.js
@@ -0,0 +1,18 @@
+class localStorageHandler {
+    constructor(url) {
+        this.url = url;
+    }
+    set(data) {
+        try {
+            const dataString = JSON.stringify(data);
+            localStorage.setItem(String(this.url), dataString);
+            return true;
+        } catch(err) {
+            return false;
+        }
+    }
+    get() {
+        const dataString = localStorage.getItem(String(this.url));
+        return JSON.parse(dataString) || []
+    }
+}
